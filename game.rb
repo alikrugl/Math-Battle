e2d9be1c5e5e56ba624d5679456c2b490277b247
Alik <alikrugl11@gmail.com>
2021-03-17 11:32:32 +0200

    Added some comments

--- game.rb
+++ game.rb
@@ -1,8 +1,11 @@
 require './source/methods'
 
 include Methods
+# Greeting with the name of game
 greeting
 sleep(1)
+# Players enter their names
 names = enter_names
+# array to call method sample for the random choice of the turn
 players = [Player.new(names[0]), Player.new(names[1])]
 
