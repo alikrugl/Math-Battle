161f1cf0baa6b41dc83d6122813eacaf6dd9b3a1
Alik <alikrugl11@gmail.com>
2021-03-17 11:50:42 +0200

    Added turn param

--- source/player.rb
+++ source/player.rb
@@ -1,10 +1,11 @@
 class Player
   attr_reader :name
-  attr_accessor :health
+  attr_accessor :health, :turn
 
   def initialize(name)
     @name = name
     @health = 100
+    @turn = false
   end
 
 end
