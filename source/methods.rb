06f8578c9c86f923f805014ed0ac9de02c43d133
Alik <alikrugl11@gmail.com>
2021-03-17 11:43:22 +0200

    Fixed loop

--- source/methods.rb
+++ source/methods.rb
@@ -6,12 +6,12 @@ module Methods
   def enter_names
     name1 = ''
     name2 = ''
-    while name1.length < 2
+    while name1.length <= 2
       print 'Player 1, please enter your name(more that 2 symbols): '
       name1 = gets.chomp.to_s
     end
     puts '  '
-    while name2.length < 2
+    while name2.length <= 2
       print 'Player 2, please enter your name(more that 2 symbols): '
       name2 = gets.chomp.to_s
     end
