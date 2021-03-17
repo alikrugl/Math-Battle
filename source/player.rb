db72b8ef656b66146a328350f1af5315a9649c53
Alik <alikrugl11@gmail.com>
2021-03-17 13:19:23 +0200

    Removed turn param

--- source/player.rb
+++ source/player.rb
@@ -1,11 +1,10 @@
 class Player
   attr_reader :name
-  attr_accessor :health, :turn
+  attr_accessor :health
 
   def initialize(name)
     @name = name
     @health = 100
-    @turn = false
   end
 
 end
