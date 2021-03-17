72a1b633b0196f8b2973587e69ff28a66aa77144
Alik <alikrugl11@gmail.com>
2021-03-17 11:11:32 +0200

    Added method to enter player names

--- source/methods.rb
+++ source/methods.rb
@@ -1,5 +1,20 @@
 module Methods
-    def greeting 
-        puts "Welcome to the Math-battle game!!!"
+  def greeting
+    puts 'Welcome to the Math-battle game!!!'
+  end
+
+  def enter_names
+    name1 = ''
+    name2 = ''
+    while name1.length < 2
+      print 'Player 1, please enter your name(more that 2 symbols): '
+      name1 = gets.chomp.to_s
     end
-end 
+    puts '  '
+    while name2.length < 2
+      print 'Player 2, please enter your name(more that 2 symbols): '
+      name2 = gets.chomp.to_s
+    end
+    [name1, name2]
+  end
+end
